7f29aed8a31e4b38d163a17f8179a1280af9aba6
Poonam Pande <p.pande@earthlink.net>
2023-10-19 10:15:57 -0400

    WIP

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 const display = document.getElementById('display');
 const buttons = Array.from(document.getElementsByClassName('calculator'));
     buttons.map( button => {
-    button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
+    button.addEventListener('click', (event) => {
+        switch(event.target.innerText){
             case 'C':
                 display.innerText = '';
                 break;
             case '=':
                 try{
                     display.innerText = eval(display.innerText);
-                } catch {
-                    display.innerText = "Error"
+                } 
+                catch {
+                    display.innerText = "Error!"
                 }
                 break;
             default:
-                display.innerText += e.target.innerText;
+                display.innerText += event.target.innerText;
         }
     });
 });
