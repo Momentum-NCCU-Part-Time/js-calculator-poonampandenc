828a94d216725ea717f224bab2fee60bd1495ea1
Poonam Pande <p.pande@earthlink.net>
2023-10-19 01:40:06 -0400

    WIP

--- main.js
+++ main.js
@@ -13,11 +13,6 @@ const buttons = Array.from(document.getElementsByClassName('calculator'));
                     display.innerText = "Error"
                 }
                 break;
-            case '←':
-                if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
-                }
-                break;
             default:
                 display.innerText += e.target.innerText;
         }
